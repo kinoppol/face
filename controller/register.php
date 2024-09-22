4add71e224085a9fa27c8a9193244484bcc01556
kinoppol <kinoppol@hotmail.com>
2024-09-22 20:56:32 +0700

    remove unused files

--- controller/register.php
+++
@@ -1,7 +0,0 @@
-<?php
-class register{
-    function index(){
-        $content=view('register/form',array('action_url'=>site_url('register/check')));
-        return view('template/authen',array('content'=>$content,'title'=>'ลงทะเบียน'));
-    }
-  }
--- view/_menu/main_menu.php
+++
@@ -1,7 +0,0 @@
-<?php
-helper('sneat/menu');
-$menu='';
-$menu.=view('_menu/admin');
-$menu.=view('_menu/budget');
-$menu.=view('_menu/project');
-print $menu;
